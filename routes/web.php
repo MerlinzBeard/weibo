d1be3eb0fbce1f3bb9bf11e375bf0e481bb56e08
Charles <yichengcharles@qq.com>
2019-09-17 03:14:37 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup'); /*注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法*/
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
