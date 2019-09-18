b4352978945322e5c00c7f1522390d02de7df2fe
Charles <yichengcharles@qq.com>
2019-09-18 00:45:52 +0000

    用户激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,24 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store', 'index']
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
 
         $this->middleware('guest', [
             'only' => ['create']
         ]);
     }
+
     public function index()
     {
         $users = User::paginate(10);
         return view('users.index', compact('users'));
     }
+
     public function create()
     {
         return view('users.create');
@@ -32,6 +35,7 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
     public function destroy(User $user)
     {
         $this->authorize('destroy', $user);
@@ -39,6 +43,7 @@ class UsersController extends Controller
         session()->flash('success', '成功删除用户！');
         return back();
     }
+
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -53,9 +58,9 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
     }
 
     public function edit(User $user)
@@ -82,5 +87,32 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->id);
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'summer@example.com';
+        $name = 'Summer';
+        $to = $user->email;
+        $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup', 'UsersController@create')->name('signup'); /*注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法*/
+Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
